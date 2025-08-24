8b212efe225102dc18e87b0dfe116d69b296dc69
권장순 <jsoonworld@gmail.com>
2025-08-24 14:42:41 +0900

    fix(security): 이벤트 API 접근 권한 인증 필요로 변경

--- src/main/java/com/terning/farewell_server/auth/jwt/SecurityConfig.java
+++ src/main/java/com/terning/farewell_server/auth/jwt/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/event/**").permitAll()
+                        .requestMatchers("/api/event/**").authenticated()
                         .anyRequest().authenticated()
                 )
 
