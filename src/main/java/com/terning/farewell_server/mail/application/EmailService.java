dbd3fc97b371bc69d33daa0c055d83e24a0ca306
권장순 <jsoonworld@gmail.com>
2025-09-07 22:27:42 +0900

    @Async를 이용한 비동기 처리로 스레드 누수 문제 해결

--- src/main/java/com/terning/farewell_server/mail/application/EmailService.java
+++ src/main/java/com/terning/farewell_server/mail/application/EmailService.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -35,6 +36,7 @@ public class EmailService {
         sendEmail(toEmail, VERIFICATION_EMAIL_SUBJECT, htmlContent);
     }
 
+    @Async("mailExecutor")
     public void sendConfirmationEmail(String toEmail) {
         String targetEmail = toEmail;
 
